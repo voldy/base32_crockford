48e049730cd13b9200a16599d9eb4b3823bbc680
Vladimir Zhukov <voldy@users.noreply.github.com>
2018-04-30 14:44:13 +0200

    Check symbol support (#1)
    implemented check symbol support

--- test/base32_crockford_test.exs
+++ test/base32_crockford_test.exs
@@ -5,8 +5,17 @@ defmodule Base32CrockfordTest do
 
   describe "#encode" do
     test "encodes integer to base 32 string" do
+      assert "0" == encode(0)
       assert "Z" == encode(31)
       assert "16J" == encode(1234)
+      assert "1CZ" == encode(1439)
+    end
+
+    test "appends check symbol" do
+      assert "00" == encode(0, check_symbol: true)
+      assert "ZZ" == encode(31, check_symbol: true)
+      assert "16JD" == encode(1234, check_symbol: true)
+      assert "1CZ~" == encode(1439, check_symbol: true)
     end
   end
 
@@ -18,6 +27,7 @@ defmodule Base32CrockfordTest do
 
     test "case insensitive" do
       assert {:ok, 31} == decode("z")
+      assert {:ok, 1439} == decode("1cz")
     end
 
     test "error resistant" do
@@ -30,6 +40,16 @@ defmodule Base32CrockfordTest do
       assert :error == decode("U")
       assert :error == decode("FU")
       assert {:ok, _} = decode("BAR")
+      assert :error == decode("1CZ~")
+    end
+
+    test "checks encoded string with check symbol" do
+      assert {:ok, 31} == decode("ZZ", check_symbol: true)
+      assert {:ok, 1439} == decode("1CZ~", check_symbol: true)
+    end
+
+    test "returns error when wrong check symbol" do
+      assert :error == decode("1CZ*", check_symbol: true)
     end
   end
 end
